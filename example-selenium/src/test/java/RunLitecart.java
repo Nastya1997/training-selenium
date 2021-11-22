1efadd8734ad0b597b7b9d3449d6edc98edfd00e
anastasiya_moskalec@mail.ru <Payrol=33>
2021-11-22 18:34:30 +0300

    running different browsers

--- example-selenium/src/test/java/RunLitecart.java
+++ example-selenium/src/test/java/RunLitecart.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RunLitecart {
@@ -13,8 +15,23 @@ public class RunLitecart {
 
     @BeforeAll
     public static void start() {
-        System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
-        webDriver = new ChromeDriver();
+        String browserName = "edge";
+        switch (browserName) {
+            case ("chrome"):
+                System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
+                webDriver = new ChromeDriver();
+                break;
+            case ("firefox"):
+                System.setProperty("webdriver.gecko.driver", "lib\\geckodriver.exe");
+                webDriver = new FirefoxDriver();
+                break;
+            case ("edge"):
+                System.setProperty("webdriver.edge.driver", "lib\\msedgedriver.exe");
+                webDriver = new EdgeDriver();
+                break;
+            default:
+                break;
+        }
         wait = new WebDriverWait(webDriver, 10);
     }
 
